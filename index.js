6f1edff228d1cacc288833df833bdfd5dc2a9358
Norman Smart <normansmart14@gmail.com>
2022-05-24 08:59:34 -0400

    commit tree 865c53d8678208eab31fbdc5ebdb9d8fa952db8c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+main.remove(main);
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+
+newHeader.innerHTML = "<p> Norman is the champion</p>";
+
+
